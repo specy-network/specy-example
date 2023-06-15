68bd00d32c17ba1ab5b94e3d9982cda317336ef7
shaojie <1654574171@qq.com>
2023-06-15 02:58:28 -0700

    improve manifest

--- manifests/rewards-manifest/src/mapping.ts
+++ manifests/rewards-manifest/src/mapping.ts
@@ -1,5 +1,5 @@
 import { BigInt, cosmos, log, store, crypto, ByteArray } from "@graphprotocol/graph-ts";
-import { rewardlist } from "../generated/schema";
+import { merkellist, rewardlist } from "../generated/schema";
 
 
 export function handleSetRewardList(data: cosmos.EventData): void {
@@ -7,5 +7,14 @@ export function handleSetRewardList(data: cosmos.EventData): void {
   let list = new rewardlist("reward_list")
   list.list = data.event.getAttributeValue("reward_list").split(",");
   log.info("handle reward list successed ,list:{}", [data.event.getAttributeValue("reward_list")])
+  list.save()
+}
+
+
+export function handleSetMerkelList(data: cosmos.EventData): void {
+
+  let list = new merkellist(data.block.header.height.toString())
+  list.root = data.event.getAttributeValue("merkel_root");
+  log.info("handle merker list successed ,list:{}", [data.event.getAttributeValue("merkel_root")])
   list.save()
 }
