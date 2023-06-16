6d07b04f84d536ae34ed8be48c4bf26c3c996aff
shaojie <1654574171@qq.com>
2023-06-16 00:20:10 -0700

    add claim list entity

--- manifests/rewards-manifest/src/mapping.ts
+++ manifests/rewards-manifest/src/mapping.ts
@@ -1,5 +1,5 @@
-import { BigInt, cosmos, log, store, crypto, ByteArray } from "@graphprotocol/graph-ts";
-import { merkellist, rewardlist } from "../generated/schema";
+import { BigInt, cosmos, log, store, crypto, ByteArray, bigInt } from "@graphprotocol/graph-ts";
+import { claimlist, merkellist, rewardlist } from "../generated/schema";
 
 
 export function handleSetRewardList(data: cosmos.EventData): void {
@@ -17,5 +17,15 @@ export function handleSetMerkelList(data: cosmos.EventData): void {
   list.root = data.event.getAttributeValue("merkel_root");
   list.timestamp = bigInt.fromString(data.block.header.time.seconds.toString())
   log.info("handle merker list successed ,list:{}", [data.event.getAttributeValue("merkel_root")])
+  list.save()
+}
+
+export function handleClaimList(data: cosmos.EventData): void {
+
+  let list = new claimlist(data.event.getAttributeValue("claim_address") + "-" + data.block.header.time.seconds.toString())
+  list.address = data.event.getAttributeValue("claim_address");
+  list.timestamp = bigInt.fromString(data.block.header.time.seconds.toString())
+  list.status = data.event.getAttributeValue("claim_status");
+  log.info("handle claim  successed ,address:{}", [data.event.getAttributeValue("claim_address")])
   list.save()
 }
